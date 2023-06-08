0a7533af4c67ccfa8aafd8c4b30330144638fece
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-06-07 22:48:09 -0600

    I beleive I fixed the repo issue and should be commiting to the right repe

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/register", authController.register);
-router.post("/", authController.login);
-
-module.exports = router;
