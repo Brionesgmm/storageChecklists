7f832dfb497a73b96cf052819c472deaf026d8fb
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-06-07 22:24:30 -0600

    git was commiting to the wrong repo'

---
+++ frontend/src/routes/Root.jsx
@@ -0,0 +1,36 @@
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import Messages from "../components/Messages";
+import { useEffect } from "react";
+
+function Root() {
+  const [user, setUser] = useState();
+  const [messages, setMessages] = useState({});
+  console.log(user);
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((res) => setUser(res.user));
+  }, []);
+
+  return (
+    <>
+      <header className="container siteHeader">
+        <div className="centerContainer">
+          <h1>
+            <Link to={user ? "/profile" : "/"}>Matt's District Facilities</Link>
+          </h1>
+        </div>
+        {user && (
+          <Link to="/logout" className="btn btn-primary logout">
+            Logout
+          </Link>
+        )}
+      </header>
+      <Messages messages={messages} />
+      <Outlet context={{ user, setUser, setMessages }} />
+    </>
+  );
+}
+
+export default Root;
