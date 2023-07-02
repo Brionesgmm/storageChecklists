f9d11140b91c1424bebccae00e6dfe3005b18ef8
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-07-01 23:21:52 -0600

    The button changes to green when you are clicking the buttons in the adminNav letting the user know which page they are on

--- frontend/src/components/AdminNav.jsx
+++ frontend/src/components/AdminNav.jsx
@@ -1,44 +1,31 @@
 import { React, useState } from "react";
-import { Link, Outlet, useOutletContext } from "react-router-dom";
+import { Link, Outlet, useOutletContext, useLocation } from "react-router-dom";
 
 const AdminNav = () => {
   const { user, setMessages } = useOutletContext();
-  const [isActive, setIsActive] = useState({
-    facilitiesActive: true,
-    pastTasksActive: false,
-    employeesActiveActive: false,
-  });
-  function changeTab(key) {
-    setIsActive({
-      facilitiesActive: key === "tasksActive",
-      pastTasksActive: key === "notesActive",
-      employeesActive: key === "pettyActive",
-    });
+  const location = useLocation();
+
+  function activeStyle(path) {
+    return location.pathname === path ? "active" : "";
   }
+
   return (
     <div>
       <div className="tabBtns">
         <Link to="/admin">
-          <button
-            className="btn tasksBtn"
-            onClick={() => changeTab("tasksActive")}
-          >
+          <button className={`btn tasksBtn ${activeStyle("/admin")}`}>
             Past Tasks
           </button>
         </Link>
         <Link to="/admin/facilities">
           <button
-            className="btn notesBtn"
-            onClick={() => changeTab("notesActive")}
+            className={`btn notesBtn ${activeStyle("/admin/facilities")}`}
           >
             Facilities
           </button>
         </Link>
         <Link to="/admin/employees">
-          <button
-            className="btn pettyBtn"
-            onClick={() => changeTab("pettyActive")}
-          >
+          <button className={`btn pettyBtn ${activeStyle("/admin/employees")}`}>
             Employees
           </button>
         </Link>
