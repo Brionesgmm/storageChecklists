ec316471985d632bf2e73b481fc9d6f822c61a46
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-07-02 19:12:13 -0600

    I have the basic layout of the facility page where the facility info show and the employees that go with the facilities

--- frontend/src/routes/Facilities.jsx
+++ frontend/src/routes/Facilities.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 
 const Facilities = () => {
-  return <div>hello</div>;
+  const [allFacilities, setAllFacilities] = useState([]);
+  const [employeeNames, setEmployeeNames] = useState({});
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const response = await fetch("/api/facilities");
+      const data = await response.json();
+      console.log(data);
+      setAllFacilities(data);
+    };
+
+    fetchProperties();
+  }, []);
+
+  useEffect(() => {
+    const fetchEmployeeNames = async () => {
+      let newEmployeeNames = {};
+      for (const facility of allFacilities) {
+        for (const employeeId of facility.employees) {
+          const response = await fetch(`/api/formUserInfo/${employeeId}`);
+          const data = await response.json();
+          console.log(data);
+          newEmployeeNames[employeeId] = data.userName;
+        }
+      }
+      setEmployeeNames(newEmployeeNames);
+      console.log(employeeNames);
+    };
+
+    if (allFacilities.length > 0) {
+      fetchEmployeeNames();
+    }
+  }, [allFacilities]);
+
+  const facilitiesElement = allFacilities.map((facility) => {
+    const employeeElements = facility.employees.map((employeeId) => (
+      <h3>{employeeNames[employeeId]}</h3>
+    ));
+
+    return (
+      <div className="facility">
+        <h2>{facility.name}</h2>
+        <h2>{facility.address}</h2>
+        <div>{employeeElements}</div>
+      </div>
+    );
+  });
+
+  return <div>{facilitiesElement}</div>;
 };
 
 export default Facilities;
