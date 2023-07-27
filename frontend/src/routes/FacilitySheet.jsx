02c4081dc9b2447ec735c3c853a8a473ce6e5f27
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-07-26 22:00:29 -0600

    set up tabs for facility sheet

--- frontend/src/routes/FacilitySheet.jsx
+++ frontend/src/routes/FacilitySheet.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FacilitySheet = () => {
-  return <div>FacilitySheet</div>;
+  const [activeTabs, setActiveTabs] = useState({
+    contacts: true,
+    facilitySystems: false,
+    utilityVendors: false,
+  });
+
+  function changeTab(key) {
+    setIsActive({
+      tasksActive: key === "contacts",
+      notesActive: key === "facilitySystems",
+      pettyActive: key === "utilityVendors",
+    });
+  }
+
+  return (
+    <div>
+      <div className="tabBtns">
+        <button
+          className="btn contactsFS"
+          onClick={() => changeTab("contacts")}
+        >
+          Contacts
+        </button>
+        <button
+          className="btn notesBtn"
+          onClick={() => changeTab("facilitySystems")}
+        >
+          Utility/Vendors
+        </button>
+        <button
+          className="btn pettyBtn"
+          onClick={() => changeTab("utilityVendors")}
+        >
+          Facility Systems
+        </button>
+      </div>
+      <form>
+        <button className="btn submitBtn" type="submit">
+          Save Changes
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default FacilitySheet;
