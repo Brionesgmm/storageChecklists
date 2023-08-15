24525ed4d0abbdba50457d8dc8ea27de1f11ddb6
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-08-14 21:04:30 -0600

    created a file for allowing the auctions document to be created for all facilities

--- frontend/src/components/Auctions.jsx
+++ frontend/src/components/Auctions.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import { React, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import FetchingDataLoadIcon from "./FetchingDataLoadIcon";
 
 const Auctions = () => {
+  const [isActive, setIsActive] = useState({
+    currentAuctions: true,
+    endedAuctions: false,
+  });
   const { user } = useOutletContext();
 
-  return <div>Auctions</div>;
+  function changeTab(key) {
+    setIsActive({
+      currentAuctions: key === "currentAuctions",
+      endedAuctions: key === "endedAuctions",
+    });
+  }
+
+  return (
+    <div className="tabBtns">
+      <button
+        className="btn currentAuctions"
+        onClick={() => changeTab("currentAuctions")}
+      >
+        Current Auctions
+      </button>
+      <button
+        className="btn endedAuctions"
+        onClick={() => changeTab("endedAuctions")}
+      >
+        Ended Auctions
+      </button>
+    </div>
+  );
 };
 
 export default Auctions;
---
+++ utility/createAuctionSheets.js
@@ -0,0 +1,33 @@
+require("dotenv").config({ path: __dirname + "/../config/.env" });
+const mongoose = require("mongoose");
+const Facility = require("../models/Facility");
+const Auction = require("../models/Auction");
+const connectDB = require("../config/database");
+
+connectDB();
+
+Facility.find({}, (err, facilities) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  facilities.forEach((facility) => {
+    const newAuction = new Auction({
+      facilityId: facility._id,
+      // Assuming 'user' is not set right now, you might need to adjust this based on your needs
+      currentAuctions: [],
+      // You will need to add auctions to 'currentAuctions' array based on your application specifics,
+      // for this script, it's left as an empty array
+    });
+
+    newAuction.save((err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(`Auction created for facility with id ${facility._id}`);
+    });
+  });
+});
