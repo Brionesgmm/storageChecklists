f6d85b6384fb4dd0bd4a0c07966681b570463072
Juan Briones <jbrionesgmm@gmail.com>
2023-09-15 11:18:18 -0600

    starting to style the live auctions section

--- frontend/src/components/CurrentAuctions.jsx
+++ frontend/src/components/CurrentAuctions.jsx
@@ -133,150 +133,182 @@ const CurrentAuctions = ({
 
   const currentAuctionsElement = (
     <div className="currentAuctionsSection">
-      <h2>Current Auctions</h2>
-      {currentAuctions.length === 0 ? (
-        <div>No Auctions</div>
-      ) : (
-        currentAuctions.map((auction, index) => (
-          <div className="liveAuctions" key={auction.id}>
-            {console.log(auction.auctionEnd.time)}
-            <h1>
-              {auction.auctionEnd.time
-                ? reformatDate(auction.auctionEnd.time)
-                : ""}
-            </h1>
-            <label htmlFor={`lockCutDate-${index}`}>Lock Cut Date</label>
-            <input
-              id={`lockCutDate-${index}`}
-              type="date"
-              value={formatDate(auction.lockCutDate.time) || ""}
-              onChange={(e) => handleChange(index, "lockCutDate.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <label htmlFor={`lockCutDateCompleted-${index}`}>Completed</label>
-            <input
-              type="checkbox"
-              id={`lockCutDateCompleted-${index}`}
-              checked={auction.lockCutDate.completed || false}
-              onChange={(e) => handleChange(index, "lockCutDate.completed", e)}
-            />
-            <label htmlFor={`lienDate-${index}`}>Lien Date</label>
-            <input
-              id={`lienDate-${index}`}
-              type="date"
-              value={formatDate(auction.lienDate.time) || ""}
-              onChange={(e) => handleChange(index, "lienDate.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <label htmlFor={`lienDateCompleted-${index}`}>Completed</label>
-            <input
-              type="checkbox"
-              id={`lienDateCompleted-${index}`}
-              checked={auction.lienDate.completed || false}
-              onChange={(e) => handleChange(index, "lienDate.completed", e)}
-            />
-            <label htmlFor={`firstAdDate-${index}`}>First Ad Date</label>
-            <input
-              id={`firstAdDate-${index}`}
-              type="date"
-              value={formatDate(auction.firstAdDate.time) || ""}
-              onChange={(e) => handleChange(index, "firstAdDate.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <label htmlFor={`firstAdDateCompleted-${index}`}>Completed</label>
-            <input
-              type="checkbox"
-              id={`firstAdDateCompleted-${index}`}
-              checked={auction.firstAdDate.completed || false}
-              onChange={(e) => handleChange(index, "firstAdDate.completed", e)}
-            />
-            <label htmlFor={`secondAdDate-${index}`}>Second Ad Date</label>
-            <input
-              id={`secondAdDate-${index}`}
-              type="date"
-              value={formatDate(auction.secondAdDate.time) || ""}
-              onChange={(e) => handleChange(index, "secondAdDate.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <label htmlFor={`secondAdDateCompleted-${index}`}>Completed</label>
-            <input
-              type="checkbox"
-              id={`secondAdDateCompleted-${index}`}
-              checked={auction.secondAdDate.completed || false}
-              onChange={(e) => handleChange(index, "secondAdDate.completed", e)}
-            />
-            <label htmlFor={`auctionStart-${index}`}>Auction Start</label>
-            <input
-              id={`auctionStart-${index}`}
-              type="date"
-              value={formatDate(auction.auctionStart.time) || ""}
-              onChange={(e) => handleChange(index, "auctionStart.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <label htmlFor={`auctionStartCompleted-${index}`}>Completed</label>
-            <input
-              type="checkbox"
-              id={`auctionStartCompleted-${index}`}
-              checked={auction.auctionStart.completed || false}
-              onChange={(e) => handleChange(index, "auctionStart.completed", e)}
-            />
-            <label htmlFor={`auctionEnd-${index}`}>Auction End</label>
-            <input
-              id={`auctionEnd-${index}`}
-              type="date"
-              value={formatDate(auction.auctionEnd.time) || ""}
-              onChange={(e) => handleChange(index, "auctionEnd.time", e)}
-              onKeyDown={handleKeyPress}
-            />
-            <div>
-              <p>Auction Units</p>
-              <p>Total Units {auction.auctionUnits.length}</p>
-              {auction.auctionUnits.map((unit) => (
-                <div key={unit.id}>
-                  {unit.unit}
+      <h1>Current Auctions</h1>
+      <div className="currentAuctions">
+        {currentAuctions.length === 0 ? (
+          <div>No Auctions</div>
+        ) : (
+          currentAuctions.map((auction, index) => (
+            <div className="liveAuctions" key={auction.id}>
+              {console.log(auction.auctionEnd.time)}
+              <div className="auctionInfo">
+                <h2 className="auctionTitle">
+                  {auction.auctionEnd.time
+                    ? reformatDate(auction.auctionEnd.time)
+                    : ""}
+                </h2>
+                <div className="auctionInputs">
+                  <label htmlFor={`lockCutDate-${index}`}>Lock Cut Date</label>
+                  <input
+                    id={`lockCutDate-${index}`}
+                    type="date"
+                    value={formatDate(auction.lockCutDate.time) || ""}
+                    onChange={(e) => handleChange(index, "lockCutDate.time", e)}
+                    onKeyDown={handleKeyPress}
+                  />
+                  <label htmlFor={`lockCutDateCompleted-${index}`}>
+                    Completed
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`lockCutDateCompleted-${index}`}
+                    checked={auction.lockCutDate.completed || false}
+                    onChange={(e) =>
+                      handleChange(index, "lockCutDate.completed", e)
+                    }
+                  />
+                  <label htmlFor={`lienDate-${index}`}>Lien Date</label>
+                  <input
+                    id={`lienDate-${index}`}
+                    type="date"
+                    value={formatDate(auction.lienDate.time) || ""}
+                    onChange={(e) => handleChange(index, "lienDate.time", e)}
+                    onKeyDown={handleKeyPress}
+                  />
+                  <label htmlFor={`lienDateCompleted-${index}`}>
+                    Completed
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`lienDateCompleted-${index}`}
+                    checked={auction.lienDate.completed || false}
+                    onChange={(e) =>
+                      handleChange(index, "lienDate.completed", e)
+                    }
+                  />
+                  <label htmlFor={`firstAdDate-${index}`}>First Ad Date</label>
+                  <input
+                    id={`firstAdDate-${index}`}
+                    type="date"
+                    value={formatDate(auction.firstAdDate.time) || ""}
+                    onChange={(e) => handleChange(index, "firstAdDate.time", e)}
+                    onKeyDown={handleKeyPress}
+                  />
+                  <label htmlFor={`firstAdDateCompleted-${index}`}>
+                    Completed
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`firstAdDateCompleted-${index}`}
+                    checked={auction.firstAdDate.completed || false}
+                    onChange={(e) =>
+                      handleChange(index, "firstAdDate.completed", e)
+                    }
+                  />
+                  <label htmlFor={`secondAdDate-${index}`}>
+                    Second Ad Date
+                  </label>
+                  <input
+                    id={`secondAdDate-${index}`}
+                    type="date"
+                    value={formatDate(auction.secondAdDate.time) || ""}
+                    onChange={(e) =>
+                      handleChange(index, "secondAdDate.time", e)
+                    }
+                    onKeyDown={handleKeyPress}
+                  />
+                  <label htmlFor={`secondAdDateCompleted-${index}`}>
+                    Completed
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`secondAdDateCompleted-${index}`}
+                    checked={auction.secondAdDate.completed || false}
+                    onChange={(e) =>
+                      handleChange(index, "secondAdDate.completed", e)
+                    }
+                  />
+                  <label htmlFor={`auctionStart-${index}`}>Auction Start</label>
+                  <input
+                    id={`auctionStart-${index}`}
+                    type="date"
+                    value={formatDate(auction.auctionStart.time) || ""}
+                    onChange={(e) =>
+                      handleChange(index, "auctionStart.time", e)
+                    }
+                    onKeyDown={handleKeyPress}
+                  />
+                  <label htmlFor={`auctionStartCompleted-${index}`}>
+                    Completed
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`auctionStartCompleted-${index}`}
+                    checked={auction.auctionStart.completed || false}
+                    onChange={(e) =>
+                      handleChange(index, "auctionStart.completed", e)
+                    }
+                  />
+                  <label htmlFor={`auctionEnd-${index}`}>Auction End</label>
+                  <input
+                    id={`auctionEnd-${index}`}
+                    type="date"
+                    value={formatDate(auction.auctionEnd.time) || ""}
+                    onChange={(e) => handleChange(index, "auctionEnd.time", e)}
+                    onKeyDown={handleKeyPress}
+                  />
+                  <div>
+                    <p>Auction Units</p>
+                    <p>Total Units {auction.auctionUnits.length}</p>
+                    {auction.auctionUnits.map((unit) => (
+                      <div key={unit.id}>
+                        {unit.unit}
+                        <button
+                          type="button"
+                          onClick={() => handleDeleteUnit(index, unit.id)}
+                        >
+                          Delete Unit
+                        </button>
+                      </div>
+                    ))}
+                    <input
+                      type="text"
+                      value={newUnit}
+                      onChange={(e) => setNewUnit(e.target.value)}
+                      placeholder="Add Unit"
+                      onKeyDown={handleKeyPress}
+                    />
+                    <button type="button" onClick={() => addUnit(index)}>
+                      Add Unit
+                    </button>
+                  </div>
+                  <label>
+                    Auction Finished:
+                    <input
+                      type="checkbox"
+                      checked={auction.auctionFinished || false}
+                      onChange={(e) =>
+                        handleChange(index, "auctionFinished", {
+                          target: { value: e.target.checked },
+                        })
+                      }
+                      onKeyDown={handleKeyPress}
+                    />
+                  </label>
                   <button
                     type="button"
-                    onClick={() => handleDeleteUnit(index, unit.id)}
+                    onClick={() =>
+                      handleDeleteAuction(auction.id, auction.auctionEnd.time)
+                    }
                   >
-                    Delete Unit
+                    Delete Auction🗑️
                   </button>
                 </div>
-              ))}
-              <input
-                type="text"
-                value={newUnit}
-                onChange={(e) => setNewUnit(e.target.value)}
-                placeholder="Add Unit"
-                onKeyDown={handleKeyPress}
-              />
-              <button type="button" onClick={() => addUnit(index)}>
-                Add Unit
-              </button>
+              </div>
             </div>
-            <label>
-              Auction Finished:
-              <input
-                type="checkbox"
-                checked={auction.auctionFinished || false}
-                onChange={(e) =>
-                  handleChange(index, "auctionFinished", {
-                    target: { value: e.target.checked },
-                  })
-                }
-                onKeyDown={handleKeyPress}
-              />
-            </label>
-            <button
-              type="button"
-              onClick={() =>
-                handleDeleteAuction(auction.id, auction.auctionEnd.time)
-              }
-            >
-              Delete Auction🗑️
-            </button>
-          </div>
-        ))
-      )}
+          ))
+        )}
+      </div>
       <button type="button" onClick={addNewAuction}>
         Add New Auction
       </button>
