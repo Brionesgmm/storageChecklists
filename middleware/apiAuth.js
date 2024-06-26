26dba20d2fc1d0304cddfc6836e680354469d7cd
unknown <jbrionesgmm@gmail.com>
2024-06-26 10:33:12 -0600

    setting up cronjob

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -1,6 +1,18 @@
+// function checkApiKey(req, res, next) {
+//   const cyclic = req.get("x-cyclic");
+//   if (cyclic !== "cron") {
+//     return res.status(401).send("Unauthorized");
+//   }
+//   next();
+// }
+
+// module.exports = checkApiKey;
+
 function checkApiKey(req, res, next) {
-  const cyclic = req.get("x-cyclic");
-  if (cyclic !== "cron") {
+  // You can check for a header or a query parameter, depending on your preference
+  const token = req.get("Authorization") || req.query.token;
+
+  if (token !== process.env.CRON_SECRET_TOKEN) {
     return res.status(401).send("Unauthorized");
   }
   next();
